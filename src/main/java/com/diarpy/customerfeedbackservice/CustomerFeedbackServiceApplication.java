d39ab0df8274c4f51d2741ee35f2554d140b3830
Rutzno <d.rutzno@gmail.com>
2024-05-13 01:49:50 +0000

    initial commit

---
+++ src/main/java/com/diarpy/customerfeedbackservice/CustomerFeedbackServiceApplication.java
@@ -0,0 +1,19 @@
+package com.diarpy.customerfeedbackservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Mack_TB
+ * @since 10/05/2024
+ * @version 1.0.1
+ */
+
+@SpringBootApplication
+public class CustomerFeedbackServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomerFeedbackServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/diarpy/customerfeedbackservice/CustomerFeedbackServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.diarpy.customerfeedbackservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerFeedbackServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

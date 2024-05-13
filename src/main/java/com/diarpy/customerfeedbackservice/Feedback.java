eda0e0fb820e2d04388e95d76589e09e787d4682
Rutzno <d.rutzno@gmail.com>
2024-05-13 01:59:50 +0000

    add Feedback.java

---
+++ src/main/java/com/diarpy/customerfeedbackservice/Feedback.java
@@ -0,0 +1,72 @@
+package com.diarpy.customerfeedbackservice;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+/**
+ * @author Mack_TB
+ * @since 10/05/2024
+ * @version 1.0.1
+ */
+
+@Document(collection = "feedback")
+public class Feedback {
+    @Id
+    private String id;
+    private int rating;
+    private String feedback; // Optional
+    private String customer; // Optional
+    private String product;
+    private String vendor;
+
+    public Feedback() {
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public int getRating() {
+        return rating;
+    }
+
+    public void setRating(int rating) {
+        this.rating = rating;
+    }
+
+    public String getFeedback() {
+        return feedback;
+    }
+
+    public void setFeedback(String feedback) {
+        this.feedback = feedback;
+    }
+
+    public String getCustomer() {
+        return customer;
+    }
+
+    public void setCustomer(String customer) {
+        this.customer = customer;
+    }
+
+    public String getProduct() {
+        return product;
+    }
+
+    public void setProduct(String product) {
+        this.product = product;
+    }
+
+    public String getVendor() {
+        return vendor;
+    }
+
+    public void setVendor(String vendor) {
+        this.vendor = vendor;
+    }
+}

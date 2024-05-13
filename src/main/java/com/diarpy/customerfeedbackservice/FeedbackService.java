28d7a8251e41365403314bac9fa4f7e687b41dc0
Rutzno <d.rutzno@gmail.com>
2024-05-13 02:05:16 +0000

    add FeedbackService.java

--- src/main/java/com/diarpy/customerfeedbackservice/FeedbackService.java
+++ src/main/java/com/diarpy/customerfeedbackservice/FeedbackService.java
@@ -3,6 +3,12 @@ package com.diarpy.customerfeedbackservice;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+/**
+ * @author Mack_TB
+ * @since 10/05/2024
+ * @version 1.0.1
+ */
+
 @Service
 public class FeedbackService {
     private final FeedbackRepository feedbackRepository;

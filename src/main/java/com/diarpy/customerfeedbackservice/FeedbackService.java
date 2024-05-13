0b5fa32bd0961878b43c6bb1c76fc4af325ee197
Rutzno <d.rutzno@gmail.com>
2024-05-13 11:31:43 +0000

    stage 2

--- src/main/java/com/diarpy/customerfeedbackservice/FeedbackService.java
+++ src/main/java/com/diarpy/customerfeedbackservice/FeedbackService.java
@@ -1,12 +1,16 @@
 package com.diarpy.customerfeedbackservice;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 /**
  * @author Mack_TB
  * @since 10/05/2024
- * @version 1.0.1
+ * @version 1.0.2
  */
 
 @Service
@@ -20,5 +24,15 @@ public class FeedbackService {
 
     public Feedback saveFeedback(Feedback feedback) {
         return feedbackRepository.save(feedback);
+    }
+
+    public ResponseEntity<Feedback> findFeedbackById(String id) {
+        Feedback feedback = feedbackRepository.findById(id).orElse(null);
+        return feedback == null ? ResponseEntity.notFound().build() : ResponseEntity.ok(feedback);
+    }
+
+    public List<Feedback> findAllFeedbacks() {
+        Sort sortById = Sort.by(Sort.Direction.DESC, "id");
+        return feedbackRepository.findAll(sortById);
     }
 }
